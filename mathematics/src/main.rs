3e175abeab2172f4ae25ffe3fc73617f21d336ec
Rodrigo Gollo <rodrigogollo.nh@gmail.com>
2024-08-09 16:47:22 -0300

    function inside parameter

--- mathematics/src/main.rs
+++ mathematics/src/main.rs
@@ -1,25 +1,28 @@
 use std::io;
 
 fn main() {
-    buzz(5);
-    buzz(7);
-    buzz(17);
-    loop {
-        println!("Please input a number.");
+    // buzz(5);
+    // buzz(7);
+    // buzz(17);
+    // loop {
+    //     println!("Please input a number.");
+    //
+    //     let mut guess = String::new();
+    //
+    //     io::stdin().read_line(&mut guess)
+    //         .expect("Failed to read line");
+    //
+    //     let guess: i32 = match guess.trim().parse() {
+    //         Ok(num) => num,
+    //         Err(_) => 0,
+    //     };
+    //
+    //     let result = abs(guess);
+    //     println!("Result: {}", result);
+    // }
 
-        let mut guess = String::new();
-
-        io::stdin().read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: i32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => 0,
-        };
-
-        let result = abs(guess);
-        println!("Result: {}", result);
-    }
+    let result = sum(cube, 3, 5);
+    println!("res: {}", result);
 }
 
 fn abs(x: i32) -> i32 {
@@ -36,4 +39,21 @@ fn buzz(x: i32) {
     } else {
         println!("{}", x);
     }
+}
+
+fn sum(func: fn(i32) -> i32, x: i32, y: i32) -> i32 {
+    if x > y {
+        return 0;
+    }
+    else {
+        return func(x) + sum(func, x + 1, y);
+    }
+}
+
+fn square(x: i32) -> i32 {
+   return x * x; 
+}
+
+fn cube(x: i32) -> i32 {
+    return x * x * x;
 }
